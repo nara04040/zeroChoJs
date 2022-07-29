46d029e257f3113ed81f61d46a886ac561820c52
Dev.Jin <nara04040@naver.com>
2022-07-29 11:26:37 +0900

    2-4까지 수업정리

---
+++ 2강/parseInt,NaN.js
@@ -0,0 +1,24 @@
+/**
+ *  parseIne :  
+ *  
+ *  NaN :
+ */
+
+
+// console.log(5e4) // 숫자 표현방식중 하나.
+// console.log(5e-4);
+// console.log(typeof NaN); // NaN은 숫자이다.
+
+
+// 문자와 숫자 더하기
+
+// console.log('문자열' + 0);
+// console.log('10' + 1);
+
+// 문자열과 숫자가 하나로 합쳐진다. 
+//  이처럼 값의 자료형을 바꾸는 행위를 "형 변환(type casting)이라고 한다."
+
+// console.log(parseInt('string'));
+// console.log('string' - 0);
+
+// 연산자를 사용할때는 숫자로 형 변환 후 빼기를 시도한다. 문자열을 숫자로 바꾸면 NaN이 되고 여기서 0 을 빼도 NaN이기에 출력내용이 NaN이 나온다. 
