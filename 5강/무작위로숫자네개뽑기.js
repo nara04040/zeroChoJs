2af47099e232513526406403954378900e89ed02
Dev.Jin <nara04040@naver.com>
2022-08-11 00:02:52 +0900

    5-6강까지

---
+++ 5강/무작위로숫자네개뽑기.js
@@ -0,0 +1,61 @@
+// 퀴즈
+// 2~5까지의 숫자를 뽑고싶다. 가 ~ 라를 채워라
+
+// 1번예제
+// const answer1 = [];
+// for (let i = 0; i < (가); i++) {
+//     answer.push(i + 2);
+// }
+
+// answer
+const answer1 = [];
+for (let i = 0; i < 4; i++) {
+    answer1.push(i + 2);
+}
+console.log(answer1)
+console.log(answer1.length)
+
+// 2번예제
+// const answer = [];
+// for (let i = 0; i <= (나); i++) {
+//     answer.push(n + 2);
+// }
+
+// answer
+const answer2 = [];
+for (let i = 0; i <= 3; i++) {
+    answer2.push(i + 2);
+}
+console.log(answer2);
+console.log(answer2.length)
+
+
+// 3번예제
+// const answer = [];
+// for (let i = 1; i < (다); i++) {
+//     answer.push(n + 1);
+// }
+
+// answer
+const answer3 = [];
+for (let i = 1; i < 5; i++) {
+    answer3.push(i + 1);
+}
+console.log(answer3);
+console.log(answer3.length)
+
+
+// 4번예제
+// const answer = [];
+// for (let i = 1; i <= (라); i++) {
+//     answer.push(n + 1);
+// }
+
+// answer
+const answer4 = [];
+for (let i = 1; i <= 4; i++) {
+    answer4.push(i + 1);
+}
+
+console.log(answer4);
+console.log(answer4.length);
